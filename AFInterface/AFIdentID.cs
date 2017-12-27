b6952993f983eb46655d6e865b0a4c89dc3ef184
flyicegood <flyicegood@163.com>
2017-12-27 23:33:16 +0800

    modify guid

--- AFInterface/AFIdentID.cs
+++ AFInterface/AFIdentID.cs
@@ -6,97 +6,97 @@ using System.Text;
 
 namespace AFCoreEx
 {
-    public class AFIDENTID : Object
+public class AFIDENTID : Object
+{
+    public Int64 nHead64;
+    public Int64 nData64;
+
+    public AFIDENTID()
     {
-        public Int32 nHead32;
-        public Int32 nData32;
+        nHead64 = 0;
+        nData64 = 0;
+    }
 
-        public AFIDENTID()
+    public AFIDENTID(AFIDENTID id)
+    {
+        nHead64 = id.nHead64;
+        nData64 = id.nData64;
+    }
+
+    public AFIDENTID(Int64 nHead, Int64 nData)
+    {
+        nHead64 = nHead;
+        nData64 = nData;
+    }
+
+    public static bool operator == (AFIDENTID ident, AFIDENTID other)
+    {
+        if(((object)ident == null) && ((object)other == null))
         {
-            nHead32 = 0;
-            nData32 = 0;
-        }
-
-        public AFIDENTID(AFIDENTID id)
-        {
-            nHead32 = id.nHead32;
-            nData32 = id.nData32;
-        }
-
-        public AFIDENTID(Int32 nHead, Int32 nData)
-        {
-            nHead32 = nHead;
-            nData32 = nData;
-        }
-
-		public static bool operator == (AFIDENTID ident, AFIDENTID other)
-		{
-            if (((object)ident == null) && ((object)other == null))
-            {
-                return true;
-            }
-
-            if (((object)ident == null) || ((object)other == null))
-            {
-                return false;
-            }
-
-            return ident.nHead32 == other.nHead32 && ident.nData32 == other.nData32;
-		}
-
-		public static bool operator != (AFIDENTID ident, AFIDENTID other)
-		{
-            return !(ident == other);
-		}
-
-        public override bool Equals(object other)
-        {
-            return this == (AFIDENTID)other;
-        }
-
-        public bool IsNull()
-        {
-            return 0 == nData32 && 0 == nHead32;
-        }
-
-        public override string ToString()
-        {
-            return nHead32.ToString() + "-" + nData32.ToString();
-        }
-
-        public bool Parse(string strData, out AFIDENTID id)
-        {
-            AFIDENTID xId = new AFIDENTID();
-            id = xId;
-
-            string[] strList = strData.Split('-');
-            if (strList.Count() != 2)
-            {
-                return false;
-            }
-
-            Int32 nHead = 0;
-            if (!Int32.TryParse(strList[0], out nHead))
-            {
-                return false;
-            }
-
-            Int32 nData = 0;
-            if (!Int32.TryParse(strList[1], out nData))
-            {
-                return false;
-            }
-
-            id.nHead32 = nHead;
-            id.nData32 = nData;
-
             return true;
         }
 
-        public override int GetHashCode()
+        if(((object)ident == null) || ((object)other == null))
         {
-            string str =this.ToString();
-            return str.GetHashCode();
+            return false;
         }
+
+        return ident.nHead64 == other.nHead64 && ident.nData64 == other.nData64;
     }
+
+    public static bool operator != (AFIDENTID ident, AFIDENTID other)
+    {
+        return !(ident == other);
+    }
+
+    public override bool Equals(object other)
+    {
+        return this == (AFIDENTID)other;
+    }
+
+    public bool IsNull()
+    {
+        return 0 == nData64 && 0 == nHead64;
+    }
+
+    public override string ToString()
+    {
+        return nHead64.ToString() + "-" + nData64.ToString();
+    }
+
+    public bool Parse(string strData, out AFIDENTID id)
+    {
+        AFIDENTID xId = new AFIDENTID();
+        id = xId;
+
+        string[] strList = strData.Split('-');
+        if(strList.Count() != 2)
+        {
+            return false;
+        }
+
+        Int64 nHead = 0;
+        if(!Int64.TryParse(strList[0], out nHead))
+        {
+            return false;
+        }
+
+        Int64 nData = 0;
+        if(!Int64.TryParse(strList[1], out nData))
+        {
+            return false;
+        }
+
+        id.nHead64 = nHead;
+        id.nData64 = nData;
+
+        return true;
+    }
+
+    public override int GetHashCode()
+    {
+        string str = this.ToString();
+        return str.GetHashCode();
+    }
+}
 }
